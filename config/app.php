345e3d7a83297ed097a63a1ea81fdae91c249e26
alpesh <alpesh.savaliya@simbanic.com>
2019-08-17 13:26:56 -0600

    config change 2

--- config/app.php
+++ config/app.php
@@ -14,6 +14,9 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
